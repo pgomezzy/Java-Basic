6ac76baa6ead2f56e08c2a65fc8960a09d86b3db
pgomezzy <palp53412@gmail.com>
2024-08-29 11:39:32 -0300

    Exemplos para estrutura for

---
+++ Controle-Fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,11 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+    
+        for (int x=0; x<=5; x++){
+            if (x == 3)
+                continue;
+            System.out.println(x);
+
+        }
+    }
+}
