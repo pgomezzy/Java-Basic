31d6f74edb14c6972554a0c9f52fe4cdbf20da9e
pgomezzy <palp53412@gmail.com>
2024-08-29 10:00:00 -0300

    Exercicio estrutara ternaria

---
+++ Controle-Fluxo/src/ResultadoEscolarTernario.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolarTernario {
+    public static void main(String[] args) throws Exception {
+
+     int nota = 7;
+
+    String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado" ;
+        System.out.println(resultado);
+
+
+
+    }
+}
