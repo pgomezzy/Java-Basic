7936ee3fe633adc02e165dcbb5eb2e915420676c
pgomezzy <palp53412@gmail.com>
2024-08-29 10:21:55 -0300

    atualização switch case

---
+++ Controle-Fluxo/src/PlanoOperadora.java
@@ -0,0 +1,29 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        // if(plano == "B"){
+        //     System.out.println("100 minutos de ligação");
+        // }else if(plano == "M"){
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whats e Instagram gratis");
+        // }else if(plano == "T"){
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whats e Instagram gratis");
+        //     System.out.println("5Gb Youtube");
+        // }
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e Instagram gratis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }     
+        }
+        
+
+    }
+}
